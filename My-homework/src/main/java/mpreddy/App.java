ad448cfc6d339c28f84bcb6e9879a269f84fd620
Prashanth <abc@gmail.com>
2017-02-22 19:20:52 -0500

    prashanth

---
+++ My-homework/src/main/java/mpreddy/App.java
@@ -0,0 +1,13 @@
+package mpreddy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
